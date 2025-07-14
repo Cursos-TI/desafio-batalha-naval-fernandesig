22a1551fd9f804ac89003fce8ca1d5a560868dba
Igor Fernandes <63010702+fernandesig@users.noreply.github.com>
2025-07-14 01:37:56 +0000

    Nivel novato - Concluído.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -6,9 +6,83 @@
 
 int main() {
     // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    int tabuleiro[10][10];
+
+    char tabela[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+
+    int navioHori[3]; // navio horizontal
+    int navioVert[3];     // navio vertical
+
+    // Preenchendo tabuleiro
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            tabuleiro[i][j] = 0;
+        }
+    }
+
+    // navio horizontal
+    int linhaHori = 1;
+    int colunaHori = 2;
+
+    // verificação limites
+    if (colunaHori + 3 > 10) {
+        printf("Erro: navio vermelho ultrapassa o limite do tabuleiro.\n");
+        return 1;
+    }
+
+    // posicionando navio horizontal
+    for (int i = 0; i < 3; i++) {
+        if (tabuleiro[linhaHori][colunaHori + i] == 3) {
+            printf("Erro: navio vermelho sobreposto a outro navio.\n");
+            return 1;
+        }
+
+        tabuleiro[linhaHori][colunaHori + i] = 3;
+        navioHori[i] = colunaHori + i;
+    }
+
+    // navio vertical
+    int linhaVert = 5;
+    int colunaVert = 5;
+
+    // limite tabuleiro
+    if (linhaVert + 3 > 10) {
+        printf("Erro: navio azul ultrapassa o limite do tabuleiro.\n");
+        return 1;
+    }
+
+    // posicao navio vertical
+    for (int i = 0; i < 3; i++) {
+        if (tabuleiro[linhaVert + i][colunaVert] == 3) {
+            printf("Erro: navio azul sobreposto a outro navio.\n");
+            return 1;
+        }
+
+        tabuleiro[linhaVert + i][colunaVert] = 3;
+        navioVert[i] = linhaVert + i;
+    }
+
+    //tabuleiro
+    printf("\nTabuleiro Batalha Naval:\n\n");
+
+    // Cabeçalho 
+    printf("    ");
+    for (int i = 0; i < 10; i++) {
+        printf(" %c ", tabela[i]);
+    }
+    printf("\n");
+
+    // linhas
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", i + 1); // Número da linha (1 a 10)
+        for (int j = 0; j < 10; j++) {
+            printf(" %d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
@@ -36,5 +110,4 @@ int main() {
     // 1 1 1 1 1
     // 0 0 1 0 0
 
-    return 0;
-}
+  
